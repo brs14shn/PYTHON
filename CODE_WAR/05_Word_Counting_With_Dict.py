e8efb425019d371a3f1909c6b3e38ebb688ce83a
brs14shn <brs>
2022-04-10 22:13:29 +0300

    file was added

---
+++ CODE_WAR/05_Word_Counting_With_Dict.py
@@ -0,0 +1,20 @@
+
+
+"""Find the numbers of an entered word with the dictionary"""
+
+
+word=input("Enter your a word: ")
+#I scream, you scream, we all scream for ice cream.
+
+liste=[]
+
+liste=word.split()
+frequency=[liste.count(i) for i in liste]
+
+myDict=dict(zip(liste,frequency))
+print("Word_Number: ",myDict)
+
+#Output==>Word_Number:  {'I': 1, 'scream,': 2, 'you': 1, 'we': 1, 'all': 1, 'scream': 1, 'for': 1, 'ice': 1, 'cream.': 1}
+
+
+
