dc8d8f1ef802c5829dbb2738a83220a795a7c783
brs14shn <brs>
2022-04-11 23:00:25 +0300

    file was added.

---
+++ CODE_WAR/07_Repeat_Elements.py
@@ -0,0 +1,14 @@
+
+
+"""
+How do we test whether there are identical digits in a number sequence?
+"""
+
+liste=[1,2,3,4,5,6]
+
+for k in liste:
+  if liste.count(k)>=2:
+    print("There are repeating elements",k)
+    break
+  else:
+   pass
