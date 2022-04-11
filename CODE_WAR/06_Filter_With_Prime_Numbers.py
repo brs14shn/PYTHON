bfb09c79b15edd92aead46469c92ed3e0db78086
brs14shn <brs>
2022-04-11 18:15:03 +0300

    file was added

---
+++ CODE_WAR/06_Filter_With_Prime_Numbers.py
@@ -0,0 +1,12 @@
+
+#all==>- all(iterable => Boolean sonuç döndürür. 
+# Bütün values true ise veya boş ise true döndürür. Haricinde false döndürür.
+#filter(Function,iterable)
+
+"""Filter ve all fonksiyonları ile asal sayı bulma"""
+
+
+
+primes=list(filter(lambda x:all(x%y!=0 for y in range(2,x)),range(2,10)))
+
+#Output==>[2,3,5,7]
