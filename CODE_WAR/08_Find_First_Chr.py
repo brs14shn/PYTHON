c4b908e156ca6d0736daccde4e634491d1af7a46
brs14shn <brs>
2022-04-13 21:05:34 +0300

    file was updated.

---
+++ CODE_WAR/08_Find_First_Chr.py
@@ -0,0 +1,25 @@
+
+"""
+Algorithm that combines the first letters 
+of the fruits in the list and makes a string 
+output='ABPAO'
+"""
+
+"SOLUTION-1"
+#map_function(function,iterable)
+
+fruit = ["Apple", "Banana", "Pear", "Apricot", "Orange"]
+for i in map(lambda x: x[0:1],fruit):
+  print(i,end="")
+
+
+"SOLUTOIN-2"
+#The join() method takes all items in an iterable and joins 
+# them into one string.
+
+fruit = ["Apple", "Banana", "Pear", "Apricot", "Orange"]
+
+first_letter=(map(lambda x: x[:1],fruit))
+
+word="".join(first_letter)
+print(word)
