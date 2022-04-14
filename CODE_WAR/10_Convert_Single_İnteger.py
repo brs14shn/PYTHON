840011dc653211c8181910fb229d00633aed3f0f
barış şahin <99876715+brs14shn@users.noreply.github.com>
2022-04-14 17:30:30 +0300

    Update 10_Convert_Single_İnteger.py

--- CODE_WAR/10_Convert_Single_İnteger.py
+++ CODE_WAR/10_Convert_Single_İnteger.py
@@ -3,12 +3,15 @@
 
 ##Convert Single İnteger
 
+
 """Convert a list of multiple integers into a single integer"""
 
 liste=[11,22,33] #except output:112233
 
 single_int=int("".join(map(str,liste)))
 print(single_int)
+
+#Output:112233
 
 
 
