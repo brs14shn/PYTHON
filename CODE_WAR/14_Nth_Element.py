3727820e024ea7cc59054d20fbe1da1734feb467
brs14shn <brs>
2022-04-16 01:58:36 +0300

    file was added

---
+++ CODE_WAR/14_Nth_Element.py
@@ -0,0 +1,15 @@
+
+
+""" How do you split a list on every Nth element."""
+
+#Sample list: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n']
+
+liste = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n']
+def list_slice(S, step):
+    return [S[i::step] for i in range(step)]
+print(list_slice(liste,3))
+
+
+#Expected Output: [['a', 'd', 'g', 'j', 'm'], ['b', 'e', 'h', 'k', 'n'], ['c', 'f', 'i', 'l']]
+
+
