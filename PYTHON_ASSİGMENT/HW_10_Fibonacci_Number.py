ed94f8bb2441b57e8cf99dfe642af5aaf5e8be9f
BARIŞ ŞAHİN <99876715+brs14shn@users.noreply.github.com>
2022-04-17 12:36:07 +0300

    Update HW_10_Fibonacci_Number.py

--- PYTHON_ASSİGMENT/HW_10_Fibonacci_Number.py
+++ PYTHON_ASSİGMENT/HW_10_Fibonacci_Number.py
@@ -3,11 +3,31 @@
   
 
 **The Fibonacci sequence is a sequence of numbers formed by adding each number with the previous one.**
+#SOLUTİON-1
 
+num = int(input())
+n1,n2 = 0,1
+count = 0
 
+if num == 1:
+    print(n1)
 
+while count < num:
+    print(n1)
+    nth = n1 + n2
+    n1 = n2
+    n2 = nth
+    count += 1
 
-fibo=[1,1]
-for i in range(8):
-  fibo.append(fibo[i]+fibo[i+1])
-fibo
+#SOLUTİON-2
+n=int(input()) 
+def fibonacci(n):
+    if n == 0:
+      return 0
+    elif n == 1:
+      return 1
+    else:
+      return fibonacci(n-1)+fibonacci(n-2)
+for n in range(n):
+   print(fibonacci(n))
+
