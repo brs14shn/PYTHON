d9898e62ae7c44e078b293dcad571aae227bbf15
brs14shn <brs>
2022-04-19 14:59:51 +0300

    file was added

---
+++ CODE_WAR/17_Recursion_With_Operations.py
@@ -0,0 +1,14 @@
+
+
+"Use recursion to calculate the sum of all items in the list"
+def calc(list):
+    if len(list)==0:
+        return 0
+    else:
+        return list[0] + calc(list[1:]) 
+
+list = [1, 3, 4, 2, 5]
+x = calc(list)        
+print(x)
+
+#Output:==>15
