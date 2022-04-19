3a4438804fd2fcfea06fbbba68ddee619f8e9619
BARIŞ ŞAHİN <99876715+brs14shn@users.noreply.github.com>
2022-04-19 15:01:53 +0300

    Update 17_Recursion_With_Operations.py

--- CODE_WAR/17_Recursion_With_Operations.py
+++ CODE_WAR/17_Recursion_With_Operations.py
@@ -11,4 +11,17 @@ list = [1, 3, 4, 2, 5]
 x = calc(list)        
 print(x)
 
-#Output:==>15
+#Output==>15
+
+
+
+"""All items in the list using recursion to calculate the sum of their squares"""
+def calc(list):
+    if len(list)==0:
+        return 0
+    else:
+        return list[0]**2 + calc(list[1:]) 
+
+list = [1, 3, 4, 2, 5]
+x = calc(list)        
+print(x)
