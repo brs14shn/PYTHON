5e5138f95fbe63ee303e8b4612139510b2599f0b
brs14shn <brs>
2022-04-22 22:48:58 +0300

    odd characters file was added.

---
+++ CODE_WAR/18_Remove_Odd _Characters.py
@@ -0,0 +1,17 @@
+
+# Remove Odd Characters in a String
+
+str1 = input("Please Enter your Own String : ")
+
+##input==>>The grass is always greener on the other side of the fence.
+
+str2 = ''
+
+for i in range(1, len(str1) + 1):
+    if(i % 2 == 0):
+        str2 = str2 + str1[i - 1]
+        
+print("Original String :  ", str1)
+print("Final String :     ", str2)
+
+#Output==>> "h rs sawy ree nteohrsd ftefne"
