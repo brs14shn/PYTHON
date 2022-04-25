bd26ed4540187aaa1332f1d4d441b081bb722452
BARIŞ ŞAHİN <99876715+brs14shn@users.noreply.github.com>
2022-04-26 02:27:48 +0300

    Update 20_Listify_the_List.py

--- CODE_WAR/20_Listify_the_List.py
+++ CODE_WAR/20_Listify_the_List.py
@@ -1,6 +1,5 @@
 """
 How to listify the list of given strings individually using Python map.
-
 """
 color = ['Red', 'Blue', 'Black', 'White', 'Pink'] 
 print("Original list: ")
@@ -10,3 +9,4 @@ result = list(map(list, color)) #map(function,iterable) list is a built-function
 print(result)
 
 #Output=>[['R', 'e', 'd'], ['B', 'l', 'u', 'e'], ['B', 'l', 'a', 'c', 'k'], ['W', 'h', 'i', 't', 'e'], ['P', 'i', 'n', 'k']]
+
